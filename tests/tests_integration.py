86c182a3b62db94ed226a96e7bf39617dbbbd0d7
CahunH <116684250+CahunH@users.noreply.github.com>
2024-08-01 15:26:04 -0600

    Update tests_integration.py

--- tests/tests_integration.py
+++ tests/tests_integration.py
@@ -1,1 +1,7 @@
+import pytest
+import request
 
+def test_index():
+  response = requests.get('<http://localhost:5000/>')
+  assert response.status_code == 200
+  assert response.json()['message'] == 'Hello, World!'
